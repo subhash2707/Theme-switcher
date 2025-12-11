5b88ff350c5d33de88917b19228ec6ff1fdce66c
subhash2707 <subhashchandrascr8433@gmail.com>
2025-12-11 20:39:30 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || "/DynamicDusk",
+  base:process.env.VITE_BASE_PATH || "/Theme-switcher",
 })
+
+// git add .
+// git commit -m "Fix"
+// git push
